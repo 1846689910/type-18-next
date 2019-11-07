5aea22fd45028f320a172f189ed1ae28ac52e116
1846689910 <eric14706@gmail.com>
2019-11-07 02:04:51 -0800

    mod

--- next.config.js
+++ next.config.js
@@ -7,17 +7,20 @@ const getShows = async () => {
 
 module.exports = {
   exportPathMap: async () => {
-    const shows = (await getShows()).reduce((p, v) => {
-      p[`/post/${v.id}`] = { page: "/post", query: { slug: v.id } };
-      return p;
-    }, {});
+    // const shows = (await getShows()).reduce((p, v) => {
+    //   p[`/post/${v.id}`] = { page: "/post", query: { slug: v.id } };
+    //   return p;
+    // }, {});
 
     return {
       "/": { page: "/" },
       "/about": { page: "/about" },
       "/secondary/my-greeting": { page: "/secondary/my-greeting" },
-      "/post/1": { page: "/post" },
-      ...shows,
+      "/post/*": { page: "/post/[postId]" },
+      "/pipelines/*": { page: "/pipelines/[pipelineId]" },
+      "/pipelines/*/states/*": {
+        page: "/pipelines/[pipelineId]/states/[stateId]"
+      }
     };
   }
 };
