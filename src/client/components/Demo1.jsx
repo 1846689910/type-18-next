4aa16a1f56f0a99d00d14000246b66ec0306af2c
1846689910 <eric14706@gmail.com>
2019-11-10 21:41:28 -0800

    add styled-components

--- src/client/components/Demo1.jsx
+++ src/client/components/Demo1.jsx
@@ -1,17 +1,25 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increase, decrease, delayIncrease } from "../settings/actions";
+import styled from "styled-components";
+
 const Demo1 = () => {
   const counter = useSelector(state => state.counter);
   const dispatch = useDispatch();
+  const StyledComponent = styled.h3`
+    color: blue;
+  `;
   return (
     <div>
       <h3>I am Dynamically loaded Component</h3>
+      <StyledComponent>I am styled-component</StyledComponent>
       <div className="d-3">
         <div id="counter">{counter.value}</div>
         <div>
           <button onClick={() => dispatch(increase())}>increase</button>
           <button onClick={() => dispatch(decrease())}>decrease</button>
-          <button onClick={() => dispatch(delayIncrease())}>delay increase</button>
+          <button onClick={() => dispatch(delayIncrease())}>
+            delay increase
+          </button>
         </div>
       </div>
       <style jsx>{`
---
+++ src/pages/_documents.js
@@ -0,0 +1,32 @@
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+class _Document extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
+export default _Document;
