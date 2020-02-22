b605ef990c0e500e305e744f8ff62a1fd87a7ee9
1846689910 <eric14706@gmail.com>
2020-02-21 20:26:41 -0800

    mod

--- test/client/index.spec.js
+++ test/client/index.spec.js
@@ -3,6 +3,7 @@ import { expect } from "chai";
 import { mount } from "enzyme";
 import { spy } from "sinon";
 import Foo from "./resources/Foo";
+import { Typography } from "@material-ui/core";
 
 spy(Foo.prototype, "componentDidMount");
 
@@ -10,6 +11,13 @@ describe("<Foo />", () => {
   it("calls componentDidMount", () => {
     const wrapper = mount(<Foo />);
     expect(wrapper.contains(<span>Hello World</span>)).to.equal(true);
+    expect(
+      wrapper.contains(
+        <Typography>
+          <span>Hello World</span>
+        </Typography>
+      )
+    ).to.equal(true);
     expect(Foo.prototype.componentDidMount).to.have.property("callCount", 1);
   });
 });
