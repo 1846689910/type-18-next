ebba39c976efe2cd19228bfd77dd2243dfd2cb59
1846689910 <eric14706@gmail.com>
2020-03-06 00:22:28 -0800

    mod

--- src/client/js/settings/actions.jsx
+++ src/client/js/settings/actions.jsx
@@ -1,23 +1,7 @@
-import Promise from "bluebird";
 export const ActionTypes = {
-  INCREASE: "INCREASE",
-  DECREASE: "DECREASE",
-  DELAY_INCREASE: "DELAY_INCREASE",
   SET_COUNTER_ACTION: "SET_COUNTER_ACTION",
   SET_SELECT_OPTIONS_ACTION: "SET_SELECT_OPTIONS_ACTION",
   SET_SELECTED_OPTION_ACTION: "SET_SELECTED_OPTION_ACTION"
-};
-export const increase = (attr={}) => ({
-  ...attr,
-  type: ActionTypes.INCREASE
-});
-export const decrease = (attr={}) => ({
-  ...attr,
-  type: ActionTypes.DECREASE
-});
-export const delayIncrease = () => async(dispatch, getState) => {  // eslint-disable-line
-  await Promise.delay(1000);
-  dispatch(increase());
 };
 /**
  * 
