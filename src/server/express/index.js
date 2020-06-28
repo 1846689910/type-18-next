d8610cea633477153f37d479da1e5e540f4bc51d
1846689910 <eric14706@gmail.com>
2020-06-27 22:11:36 -0700

    mod

--- src/server/express/index.js
+++ src/server/express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const next = require("next");
 const { graphqlMiddleware2 } = require("./graphql-middleware");
+const Status = require("http-status");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -10,6 +11,11 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 
 app.prepare().then(() => {
   const server = express();
+
+  server.get("/alive", (_, res) => {
+    res.status(Status.OK).end();
+  });
+  
   server.use(graphqlMiddleware2);
 
   server.all("*", handle);
