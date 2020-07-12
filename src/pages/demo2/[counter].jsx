519c41af7945d708a8e74752fefe33aef02b5bc9
1846689910 <eric14706@gmail.com>
2020-07-12 00:05:11 -0700

    mod

--- src/pages/demo2/[counter].jsx
+++ src/pages/demo2/[counter].jsx
@@ -1,10 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Container, Grid } from "@material-ui/core";
 import { useRouter } from "next/router";
+import { get, isNaN } from "lodash";
 import Nav from "../../client/js/components/Nav";
+import { setCounterAction } from "../../client/js/settings/actions";
 
 export default function Demo2() {
+  const dispatch = useDispatch();
+  const mainCounter = useSelector((state) => state.counter.value);
   const router = useRouter();
+  useEffect(() => {
+    const counter = parseInt(get(router, "query.counter", 0));
+    if (mainCounter !== counter && !isNaN(counter)) {
+      dispatch(setCounterAction(counter));
+    }
+  }, []);
   return (
     <Fragment>
       <Nav />
