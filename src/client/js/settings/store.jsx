b1effb5ce5547f58b9c832248bad554988c3ba3b
1846689910 <eric14706@gmail.com>
2020-07-29 22:34:35 -0700

    mod

--- src/client/js/settings/store.jsx
+++ src/client/js/settings/store.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { createStore, applyMiddleware } from "redux";
 import { reducer } from "./reducers";
 import { initialState } from "./reducers";
@@ -5,5 +6,38 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
-export const configureStore = (preloadedState = initialState) =>
-  createStore(reducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)));
+const configureStore = (preloadedState = initialState) =>
+  createStore(
+    reducer,
+    preloadedState,
+    composeWithDevTools(applyMiddleware(thunk, logger)),
+  );
+
+let store;
+
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? configureStore(preloadedState);
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = configureStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+    // Reset the current store
+    store = undefined;
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === "undefined") return _store;
+  // Create the store once in the client
+  if (!store) store = _store;
+
+  return _store;
+};
+
+export function useStore(preloadedState = initialState) {
+  const store = useMemo(() => initializeStore(preloadedState), [preloadedState]);
+  return store;
+}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import App from "next/app";
 import React from "react";
 import { Provider } from "react-redux";
-import { configureStore } from "../client/js/settings/store";
+import { useStore } from "../client/js/settings/store";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { theme } from "../client/styles/theme";
@@ -16,36 +16,54 @@ import fetch from "node-fetch";
 
 global.fetch = fetch;
 
-export default class _App extends App {
-  constructor(props){
-    super(props);
-    this.store = configureStore();
-  }
-  componentDidMount() {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }
-  static graphqlUri = "/graphql"; // default value
-  render() {
-    const { Component, pageProps } = this.props;
-    const apolloClient = new ApolloClient({
-      uri: _App.graphqlUri
-    });
-    return (
-      <Provider store={this.store}>
-        <ApolloProvider client={apolloClient}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {/* Kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <MediaQueryProvider>
-              <Component {...pageProps} />
-            </MediaQueryProvider>
-          </ThemeProvider>
-        </ApolloProvider>
-      </Provider>
-    );
-  }
+export default function App1({ Component, pageProps }){
+  const store = useStore(pageProps.initialReduxState);
+  const graphqlUri = "/graphql"; // default value
+  const apolloClient = new ApolloClient({
+    uri: graphqlUri
+  });
+  return (
+    <Provider store={store}>
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {/* Kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <MediaQueryProvider>
+            <Component {...pageProps} />
+          </MediaQueryProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </Provider>
+  );
 }
+
+// export default class _App extends App {
+//   componentDidMount() {
+//     // Remove the server-side injected CSS.
+//     const jssStyles = document.querySelector("#jss-server-side");
+//     if (jssStyles) {
+//       jssStyles.parentElement.removeChild(jssStyles);
+//     }
+//   }
+//   static graphqlUri = "/graphql"; // default value
+//   render() {
+//     const { Component, pageProps } = this.props;
+//     const store = configureStore(pageProps.initialReduxState);
+//     const apolloClient = new ApolloClient({
+//       uri: _App.graphqlUri
+//     });
+//     return (
+//       <Provider store={store}>
+//         <ApolloProvider client={apolloClient}>
+//           <ThemeProvider theme={theme}>
+//             <CssBaseline />
+//             {/* Kickstart an elegant, consistent, and simple baseline to build upon. */}
+//             <MediaQueryProvider>
+//               <Component {...pageProps} />
+//             </MediaQueryProvider>
+//           </ThemeProvider>
+//         </ApolloProvider>
+//       </Provider>
+//     );
+//   }
+// }
