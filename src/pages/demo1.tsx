2282c11f0c0f586fe7dcbf0898ab93e8870a38b1
1846689910 <eric14706@gmail.com>
2020-08-01 23:58:55 -0700

    demo1.tsx

--- src/pages/demo1.tsx
+++ src/pages/demo1.tsx
@@ -1,12 +1,17 @@
 import React, { Fragment } from "react";
-import PropTypes from "prop-types";
 import { Grid, Container } from "@material-ui/core";
 import Nav from "../client/js/components/Nav";
 import RecomposeDemo from "../client/js/components/Demo1/RecomposeDemo";
 import ReduxStateDemo from "../client/js/components/Demo1/ReduxStateDemo";
 import DemoWrapper from "../client/js/components/DemoWrapper";
 
-export default function Demo1(props) {
+type Demo1Props = {
+  initObj: {
+    message: string
+  }
+};
+
+export default function Demo1(props: Demo1Props) {
   console.log(props.initObj);
   return (
     <Fragment>
@@ -24,9 +29,6 @@ export default function Demo1(props) {
     </Fragment>
   );
 }
-Demo1.propTypes = {
-  initObj: PropTypes.object
-};
 // getInitialProps deprecated
 export async function getStaticProps() {
   const message = await Promise.resolve("Hello");
