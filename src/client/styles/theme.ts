109d86187f41ce9d87a02a5915bebf82068d3e39
1846689910 <eric14706@gmail.com>
2020-08-02 00:59:08 -0700

    theme

---
+++ src/client/styles/theme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme, Theme } from "@material-ui/core/styles";
+
+export const theme: Theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#4dabf5",
+      main: "#2196f3",
+      dark: "#1769aa",
+    },
+    secondary: {
+      light: "#f73378",
+      main: "#f50057",
+      dark: "#ab003c",
+    },
+  },
+});
