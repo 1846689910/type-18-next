51daa48e81c5f455f0c12d7b716682e5d3e5bedc
1846689910 <eric14706@gmail.com>
2020-08-02 18:36:26 -0700

    mod

--- src/client/js/models/Landmark.ts
+++ src/client/js/models/Landmark.ts
@@ -1,12 +1,10 @@
 export default class Landmark {
-  /**
-   *
-   * @param {String} name
-   * @param {Number[]} coordinates
-   * @param {String} address
-   * @param {String} url
-   * @param {String} description
-   */
+  name: string;
+  coordinates: number[];
+  address: string;
+  url: string;
+  description: string;
+  id: string;
   constructor(name, coordinates, address, url, description, id) {
     this.name = name;
     this.coordinates = coordinates;
@@ -19,23 +17,38 @@ export default class Landmark {
    * @param {Object} obj
    * @returns {Landmark}
    */
-  static fromObject = obj => {
+  static fromObject = (obj: {
+    name: string;
+    coordinates: number[];
+    address: string;
+    url: string;
+    description: string;
+    id: string;
+  }): Landmark => {
     const { name, coordinates, address, url, description, id } = obj;
     return new Landmark(name, coordinates, address, url, description, id);
   };
   /**
    * @returns {Object[]}
    */
-  toFields = () => [
+  toFields = (): {
+    label?: string;
+    key: string;
+    value: string;
+  }[] => [
     { label: "Name", key: "name", value: this.name },
     {
       label: "Coordinates",
       key: "coordinates",
-      value: this.coordinates.join(",")
+      value: this.coordinates.join(","),
     },
     { label: "Address", key: "address", value: this.address },
-    { label: "Description", key: "description", value: this.description },
+    {
+      label: "Description",
+      key: "description",
+      value: this.description,
+    },
     { label: "Url", key: "url", value: this.url },
-    { key: "id", value: this.id }
+    { key: "id", value: this.id },
   ];
-};
+}
