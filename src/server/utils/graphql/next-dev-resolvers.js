3bbba945093d9b6c3dc9b0f82a0aeb06d4991ae8
Eric He <38680030+1846689910@users.noreply.github.com>
2020-08-05 20:04:26 -0700

    [patch] update graphql query end point (#72)

--- src/server/utils/graphql/next-dev-resolvers.js
+++ src/server/utils/graphql/next-dev-resolvers.js
@@ -20,11 +20,12 @@ const localMockDataFetch = () => {
 module.exports = {
   Query: {
     hello: () => "Hello, type-18-next is now boosted by Apollo + GraphQL",
-    landmark: ({ id }) => localMockDataFetch().find(x => x.id === id),
+    landmark: (parent, { name }) =>
+      localMockDataFetch().find((x) => x.name === name),
     landmarks() {
       const landmarks = localMockDataFetch();
       return landmarks;
-    }
+    },
   },
   Mutation: {
     createLandmark(parent, { landmark }) {
@@ -32,7 +33,7 @@ module.exports = {
       const nextId =
         Math.max.apply(
           null,
-          landmarks.map(x => x.id)
+          landmarks.map((x) => x.id),
         ) + 1;
       landmark.id = `${nextId}`;
       landmarks.push(landmark);
@@ -40,17 +41,17 @@ module.exports = {
     },
     deleteLandmark(parent, { id }) {
       const landmarks = localMockDataFetch();
-      const idx = landmarks.findIndex(x => x.id === id);
+      const idx = landmarks.findIndex((x) => x.id === id);
       if (idx < 0) return null;
       const [deleted] = landmarks.splice(idx, 1);
       return deleted;
     },
     updateLandmark(parent, { id, landmark }) {
       const landmarks = localMockDataFetch();
-      const idx = landmarks.findIndex(x => x.id === id);
+      const idx = landmarks.findIndex((x) => x.id === id);
       if (idx < 0) return null;
       landmarks.splice(idx, 1, landmark);
       return landmark;
-    }
-  }
+    },
+  },
 };
